e080a5f05c6eb135c6aaf2272c4679202222fe4e
Daniel de Kok <me@danieldk.eu>
2010-09-08 23:41:56 +0200

    lbfgs: more friendly interface for the evaluation and progress functions.
    Both functions should now accept StorableArray instances.

--- Numeric/LBFGS.hs
+++ Numeric/LBFGS.hs
@@ -1,9 +1,13 @@
-module Numeric.LBFGS (LineSearchAlgorithm(..), lbfgs) where
+module Numeric.LBFGS (LineSearchAlgorithm(..), EvaluateFun,
+                      ProgressFun, lbfgs) where
 
+import Data.Array.Storable (StorableArray(..),
+                            unsafeForeignPtrToStorableArray)
 import Foreign.C.Types (CDouble, CInt)
+import Foreign.ForeignPtr (newForeignPtr_)
 import Foreign.Marshal.Alloc (malloc, free)
 import Foreign.Ptr (FunPtr, Ptr, freeHaskellFunPtr, nullPtr, plusPtr)
-import Foreign.Storable (peek, poke, sizeOf)
+import Foreign.Storable (Storable(..), peek, poke, sizeOf)
 
 import qualified Numeric.LBFGS.Raw as R
 import Numeric.LBFGS.Raw (CEvaluateFun, CProgressFun, CLBFGSParameter(..),
@@ -72,18 +76,58 @@ vectorToList_ pStart pCur l
   vectorToList_ pStart (cDoublePlusPtr pCur (-1)) (val:l)
     | otherwise = return l
 
-lbfgs :: LineSearchAlgorithm -> CEvaluateFun a -> CProgressFun a ->
-         [Double] -> IO([Double])
-lbfgs ls evalFun progressFun p = do
+
+type EvaluateFun a = (a -> StorableArray Int CDouble ->
+    StorableArray Int CDouble -> CInt -> CDouble -> IO (CDouble))
+
+wrapEvaluateFun :: (Storable a) => EvaluateFun a -> Ptr a -> Ptr CDouble ->
+                   Ptr CDouble -> CInt -> CDouble -> IO (CDouble)
+wrapEvaluateFun fun inst x g n step = do
+  let nInt = fromIntegral n
+  instV <- peek inst
+  xFp <- newForeignPtr_ x
+  xArr <- unsafeForeignPtrToStorableArray xFp (0, nInt - 1)
+  gFp <- newForeignPtr_ g
+  gArr <- unsafeForeignPtrToStorableArray gFp (0, nInt - 1)
+  fun instV xArr gArr n step
+
+type ProgressFun a = (a -> StorableArray Int CDouble ->
+                      StorableArray Int CDouble -> CDouble ->
+                      CDouble -> CDouble -> CDouble -> CInt -> CInt ->
+                      CInt -> IO (CInt))
+
+wrapProgressFun :: (Storable a) => ProgressFun a -> Ptr a -> Ptr CDouble ->
+                   Ptr CDouble-> CDouble -> CDouble -> CDouble -> CDouble ->
+                   CInt -> CInt -> CInt -> IO (CInt)
+wrapProgressFun fun inst x g fx xn gn step n k ls = do
+  let nInt = fromIntegral n
+  instV <- peek inst
+  xFp <- newForeignPtr_ x
+  xArr <- unsafeForeignPtrToStorableArray xFp (0, nInt - 1)
+  gFp <- newForeignPtr_ g
+  gArr <- unsafeForeignPtrToStorableArray gFp (0, nInt - 1)
+  fun instV xArr gArr fx xn gn step n k ls
+
+lbfgs :: (Storable a) => LineSearchAlgorithm -> EvaluateFun a ->
+         ProgressFun a -> a -> [Double] -> IO([Double])
+lbfgs ls evalFun progressFun inst p = lbfgs_ ls (wrapEvaluateFun evalFun)
+                                 (wrapProgressFun progressFun) inst p
+
+lbfgs_ :: (Storable a) => LineSearchAlgorithm -> CEvaluateFun a ->
+          CProgressFun a -> a -> [Double] -> IO([Double])
+lbfgs_ ls evalFun progressFun inst p = do
   (n, pVec) <- listToVector p
   let param = withParam ls
+  instP <- malloc
+  poke instP inst
   paramP <- malloc
   poke paramP param
   evalW <- c_lbfgs_evaluate_t_wrap evalFun
   progressW <- c_lbfgs_progress_t_wrap progressFun
-  r <- c_lbfgs n pVec nullPtr evalW progressW nullPtr paramP
+  r <- c_lbfgs n pVec nullPtr evalW progressW instP paramP
   freeHaskellFunPtr progressW
   freeHaskellFunPtr evalW
   free paramP
+  free instP
   freeVector pVec
   vectorToList n pVec
