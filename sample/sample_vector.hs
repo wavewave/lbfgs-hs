856b5dd7769563c2e3234b75f9fa419dff6363f0
Ian-Woo Kim <ianwookim@gmail.com>
2016-06-04 09:59:51 +0000

    change StorableArray to IOVector successfully

--- sample/sample_vector.hs
+++ sample/sample_vector.hs
@@ -1,4 +1,5 @@
-import Data.Array.Storable (StorableArray, readArray, writeArray)
+import Data.Vector.Storable.Mutable (IOVector,MVector(..))
+import qualified Data.Vector.Storable.Mutable as M
 import Numeric.LBFGS.Vector
          (LineSearchAlgorithm(..), LBFGSParameters(..),
           LBFGSResult, lbfgs)
@@ -16,12 +17,12 @@ import Foreign.C.Types (CDouble, CInt)
 --
 -- objective function here is f(x) = Sum_(k=0)^50 ( (1-x[2k])^2 + 10^2 (x[2k+1] - x[2k]^2)^2 )
 --
-eval :: Double -> StorableArray Int CDouble -> StorableArray Int CDouble -> CInt -> CDouble -> IO (CDouble)
+eval :: Double -> IOVector CDouble -> IOVector CDouble -> CInt -> CDouble -> IO (CDouble)
 eval inst x g n step = eval_ inst x g n step 0.0 {- initial value of sum -} 0 {- initial index i -} 
 
 eval_ :: Double                    -- ^ inst ; internal state
-      -> StorableArray Int CDouble -- ^ x: domain vector 
-      -> StorableArray Int CDouble -- ^ gradient: Del f
+      -> IOVector CDouble -- ^ x: domain vector 
+      -> IOVector CDouble -- ^ gradient: Del f
       -> CInt                      -- ^ n ( dim)  
       -> CDouble                   -- ^ step
       --------------------------------  eval up to here 
@@ -32,25 +33,25 @@ eval_ inst x g n step fx curN
   | curN >= n = return fx                                -- if i >= dim => finish
   | otherwise = do                                       -- if i < dim
       let nInt = fromIntegral curN                       -- curN = nInt = 2k
-      val <- readArray x nInt                            -- val = x[2k]
-      nextVal <- readArray x $ nInt + 1                  -- nextVal = x[2k+1]
+      val <- M.read x nInt                               -- val = x[2k]
+      nextVal <- M.read x (nInt + 1)                     -- nextVal = x[2k+1]
       let t1 = 1.0 - val
           t2 = 10.0*(nextVal - val*val)
           nFx = fx + (t1*t1 + t2*t2)
           nextGrad = 20.0*t2
-      writeArray g (nInt + 1) nextGrad                   -- g[2k+1] = nextGrad
-      writeArray g nInt $ (-2.0) * (val * nextGrad + t1) -- g[2k] = -2*(val*nextGrad + t1) 
+      M.write g (nInt + 1) nextGrad                   -- g[2k+1] = nextGrad
+      M.write g nInt $ (-2.0) * (val * nextGrad + t1) -- g[2k] = -2*(val*nextGrad + t1) 
       eval_ inst x g n step nFx (curN + 2)               -- recursion corresponding to (do ... while) 
 
 --
 -- progress is called at each step.
 --
 
-progress :: a -> StorableArray Int CDouble -> StorableArray Int CDouble
+progress :: a -> IOVector CDouble -> IOVector CDouble
          -> CDouble -> CDouble -> CDouble -> CDouble -> CInt -> CInt 
          -> CInt -> IO (CInt)
 progress _ x _ fx _ _ _ _ k _ = do
-    x0 <- readArray x 0
+    x0 <- M.read x 0
     putStrLn $ "Iteration " ++ show k ++ " :"
     putStrLn $ "fx = " ++ show fx ++ ", x[0] = " ++ show x0
     return 0
